e13bcedc98e6fa443a90170b97f52df76ed3e96b
Jiaxuan Wu <jiw4008@med.cornell.edu>
2022-10-19 23:19:08 -0400

    ddd

---
+++ R/public.R
@@ -0,0 +1,38 @@
+library("tidyverse")
+library("dplyr")
+library("usethis")
+library("devtools")
+library("roxygen2")
+
+
+try <- function(x) {
+  x+1
+}
+
+
+drg_boxplot <- function(varname, dataset="./DRG_data.csv") {
+  data <- read.csv(dataset)
+  data %>%
+    mutate(drgCode = substr(DRG.Definition, 1,3)) %>%
+    rename(groupvar = all_of(varname)) %>%
+    ggplot(aes(y=groupvar, x=drgCode)) +
+    geom_boxplot()
+}
+
+
+
+drg_med_stat <- function(stat_type, dataset="./DRG_data.csv") {
+  data = read.csv(dataset)
+  drg_stat <- data %>%
+    mutate(drgCode = substr(DRG.Definition, 1,3)) %>%
+    summarise(mean=mean(Average.Medicare.Payments),
+              sd=sd(Average.Medicare.Payments),
+              median=median(Average.Medicare.Payments))
+
+  output <- drg_stat %>%
+    rename(gvar = all_of(stat_type)) %>%
+    select(gvar)
+
+  head(output)[[1]]
+
+}
